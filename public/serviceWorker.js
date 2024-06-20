3587789fb788baa074b4c48358c7c7a77ad70785
Tommy Tracx <ttracx@icloud.com>
2024-06-19 22:38:23 -0500

    Update serviceWorker.js

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,4 @@
-const CHATGPT_NEXT_WEB_CACHE = "chatgpt-next-web-cache";
+const CHATGPT_NEXT_WEB_CACHE = "insightai-cache";
 
 self.addEventListener("activate", function (event) {
   console.log("ServiceWorker activated.");
@@ -6,7 +6,7 @@ self.addEventListener("activate", function (event) {
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CHATGPT_NEXT_WEB_CACHE).then(function (cache) {
+    caches.open(INSIGHTAI_CACHE).then(function (cache) {
       return cache.addAll([]);
     }),
   );
